7562aa4628eac4e8aa60b9405fcc8b3f73783a63
Abhishek M <abhishek.mukhopadhyay@mswipe.com>
2017-12-05 20:45:18 +0530

    DemoSIM

---
+++ DemoSim.cpp
@@ -0,0 +1,411 @@
+/*
+ * TranMgr.cpp
+ *
+ *  Created on: 14-Oct-2016
+ *      Author: abhishek
+ */
+#include <Poco/Net/ServerSocket.h>
+#include <Poco/Net/HTTPServer.h>
+#include <Poco/Net/HTTPRequestHandler.h>
+#include <Poco/Net/HTTPRequestHandlerFactory.h>
+#include <Poco/Net/HTTPResponse.h>
+#include <Poco/Net/HTTPServerRequest.h>
+#include <Poco/Net/HTTPServerResponse.h>
+#include <Poco/Util/ServerApplication.h>
+#include <Poco/Net/HTMLForm.h>
+#include <Poco/Net/PartHandler.h>
+#include <Poco/CountingStream.h>
+#include <Poco/NullStream.h>
+#include <Poco/StreamCopier.h>
+
+#include <Poco/Logger.h>
+#include <Poco/FileChannel.h>
+#include <Poco/AutoPtr.h>
+#include <Poco/FormattingChannel.h>
+#include <Poco/PatternFormatter.h>
+
+#include <Poco/JSON/Object.h>
+#include <Poco/JSON/Parser.h>
+#include <Poco/Dynamic/Var.h>
+
+#include <Poco/NumberFormatter.h>
+
+#include <Poco/Util/Option.h>
+#include <Poco/Util/OptionSet.h>
+#include <Poco/Util/HelpFormatter.h>
+
+#include <Poco/NumberParser.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <pqxx/pqxx>
+
+#include <ap/Iso8583JSON.h>
+#include <ap/constants.h>
+#include <ap/DBManager.h>
+
+using namespace Poco::Net;
+using namespace Poco::Util;
+using Poco::Net::NameValueCollection;
+using namespace std;
+using Poco::CountingInputStream;
+using Poco::NullOutputStream;
+using Poco::StreamCopier;
+
+using Poco::Util::Option;
+using Poco::Util::OptionSet;
+using Poco::Util::OptionCallback;
+using Poco::Util::HelpFormatter;
+using Poco::Logger;
+using Poco::FileChannel;
+using Poco::AutoPtr;
+using Poco::FormattingChannel;
+using Poco::PatternFormatter;
+
+using Poco::NumberFormatter;
+using Poco::NumberParser;
+
+using namespace Poco::JSON;
+using namespace Poco::Dynamic;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int authid;
+string dburl;
+string hsmurl;
+string env;
+
+class DBUpdate : public Poco::Runnable {
+public:
+
+    DBUpdate(string modulename, Logger& alogger) :
+    _modulename(modulename), m_logger(alogger) {
+    }
+
+    virtual void run() {
+        try {
+            DBManager dbm;
+
+            pqxx::connection c(dburl);
+
+            string query =
+                    "UPDATE onl_process set last_update_datetime=now() where pname='"
+                    + _modulename + "';";
+
+            while (1) {
+                pqxx::work txn(c);
+                txn.exec(query);
+                txn.commit();
+
+                sleep(30);
+            }
+        } catch (exception &e) {
+            m_logger.error(e.what());
+        }
+    }
+
+private:
+    string _modulename;
+    Logger& m_logger;
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class GetStatusHandler : public HTTPRequestHandler {
+public:
+
+    GetStatusHandler(Logger& alogger) :
+    m_logger(alogger) {
+    }
+
+    ~GetStatusHandler() {
+    }
+
+    virtual void handleRequest(HTTPServerRequest &req, HTTPServerResponse &resp) {
+        string responseStr = "OK";
+
+        resp.setStatus(HTTPResponse::HTTP_OK);
+        resp.setContentType("application/json; charset=UTF-8");
+
+        ostream& out = resp.send();
+        out << responseStr;
+
+        out.flush();
+
+        m_logger.information("Responded with OK");
+    }
+
+private:
+    Logger& m_logger;
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class RequestHandler : public HTTPRequestHandler {
+public:
+
+    RequestHandler(Logger& logger) : m_logger(logger) {
+    }
+
+    ~RequestHandler() {
+    }
+
+    virtual void handleRequest(HTTPServerRequest &req,
+            HTTPServerResponse &resp) {
+        string responseStr;
+        string requestStr;
+        istream& istr = req.stream();
+
+        StreamCopier::copyToString(istr, requestStr);
+
+        responseStr = processMsg(requestStr);
+
+        resp.setStatus(HTTPResponse::HTTP_OK);
+        resp.setContentType("application/json");
+
+        ostream& out = resp.send();
+        out << responseStr;
+
+        out.flush();
+    }
+
+private:
+    Logger& m_logger;
+
+    string processMsg(string request);
+};
+
+//////////////////////////////////////////////////////
+
+string RequestHandler::processMsg(string request) {
+    Iso8583JSON msg;
+
+    //Parser parser;
+    string resp("NOK");
+
+    try {
+        msg.parseMsg(request);
+
+        m_logger.information(msg.dumpMsg());
+
+        string mti = msg.getMsgType();
+
+        msg.setField(_038_AUTH_ID_RSP, "DEMO01");
+
+        msg.setField(_039_RSP_CODE, "00");
+
+        msg.setRspMsgType();
+
+        msg.removeField(_035_TRACK_2_DATA);
+        msg.removeField(_045_TRACK_1_DATA);
+        msg.removeField(_052_PIN_DATA);
+        msg.removeField(_055_EMV_DATA);
+
+        m_logger.information(msg.dumpMsg());
+
+        resp = msg.toMsg();
+    } catch (exception &e) {
+        m_logger.error(Poco::format("Exception : (%s)", e.what()));
+
+        msg.setRspMsgType();
+        msg.setField(_039_RSP_CODE, _96_SYSTEM_MALFUNCTION);
+
+        resp = msg.toMsg();
+    }
+
+    return resp;
+}
+
+//////////////////////////////////////////////////////
+
+class RequestHandlerFactory : public HTTPRequestHandlerFactory {
+public:
+
+    RequestHandlerFactory(Logger& alogger) :
+    m_logger(alogger) {
+    }
+
+    ~RequestHandlerFactory() {
+    }
+
+    virtual HTTPRequestHandler* createRequestHandler(
+            const HTTPServerRequest& request) {
+        if (request.getURI() == "/getstatus") {
+            return new GetStatusHandler(m_logger);
+        } else if (request.getURI() == "/updatestatus") {
+            return new GetStatusHandler(m_logger);
+        } else if (request.getURI() == "/transaction") {
+            return new RequestHandler(m_logger);
+        } else {
+            return 0;
+        }
+    }
+
+private:
+    Logger& m_logger;
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+class DemoServerApp : public ServerApplication {
+protected:
+
+    void initialize(Application& self) {
+        loadConfiguration();
+
+        ServerApplication::initialize(self);
+    }
+
+    ////////////////////////////////////
+
+    void uninitialize() {
+        ServerApplication::uninitialize();
+    }
+
+    ////////////////////////////////////
+
+    void defineOptions(OptionSet& options) {
+        ServerApplication::defineOptions(options);
+
+        options.addOption(
+                Option("help", "h", "display argument help information").required(
+                false).repeatable(false).callback(
+                OptionCallback<DemoServerApp>(this,
+                &DemoServerApp::handleHelp)));
+
+        options.addOption(
+                Option("config-file", "f", "load configuration data from a file")
+                .required(false)
+                .repeatable(true)
+                .argument("file")
+                .callback(OptionCallback<DemoServerApp>(this, &DemoServerApp::handleConfig)));
+    }
+
+    ////////////////////////////////////
+
+    void handleHelp(const string& name, const string& value) {
+        HelpFormatter helpFormatter(options());
+        helpFormatter.setCommand(commandName());
+        helpFormatter.setUsage("OPTIONS");
+        helpFormatter.setHeader("Demo SIMULATOR (Prime)");
+        helpFormatter.format(cout);
+        stopOptionsProcessing();
+        _helpRequested = true;
+    }
+
+    ////////////////////////////////////
+
+    void handleConfig(const string& name, const string& value) {
+        loadConfiguration(value);
+    }
+
+    ////////////////////////////////////
+
+    int main(const vector<string> &) {
+        if (!_helpRequested) {
+            string mq_name = config().getString("mq_name", "NOK");
+            string moduleName = config().getString("ModuleName", "NOK");
+            string path = config().getString("path", "NOK");
+            string rotation = config().getString("rotation", "NOK");
+            string archive = config().getString("archive", "NOK");
+            string ip = config().getString("IP", "127.0.0.1");
+            string times = config().getString("times", "local");
+            string compress = config().getString("compress", "true");
+            string purgeAge = config().getString("purgeAge", "2 days");
+            string purgeCount = config().getString("purgeCount", "2");
+            string loglevel = config().getString("loglevel", "information");
+            unsigned short port = (unsigned short) config().getInt("Port", 28080);
+            unsigned short maxThreads = (unsigned short) config().getInt("MaxThreads", 100);
+            unsigned short maxQueued = (unsigned short) config().getInt("MaxQueued", 100);
+            unsigned short threadIdleTime = (unsigned short) config().getInt("ThreadIdleTime", 30);
+            unsigned short maxConns = (unsigned short) config().getInt("MaxConns", 100);
+            dburl = config().getString("DBURL");
+            hsmurl = config().getString("HSMURL");
+            env = config().getString("Environment", "P");
+
+            AutoPtr<FileChannel> pChannel(new FileChannel);
+            AutoPtr<PatternFormatter> pPF(new PatternFormatter);
+            pPF->setProperty("pattern", "%Y-%m-%d %H:%M:%S %s: %t");
+            pPF->setProperty("times", times);
+
+            pChannel->setProperty("path", path);
+            pChannel->setProperty("rotation", rotation);
+            pChannel->setProperty("archive", archive);
+            pChannel->setProperty("times", times);
+
+            pChannel->setProperty("compress", compress);
+            pChannel->setProperty("purgeAge", purgeAge);
+            pChannel->setProperty("purgeCount", purgeCount);
+
+            AutoPtr<FormattingChannel> pFC(
+                    new FormattingChannel(pPF, pChannel));
+
+            Logger::root().setChannel(pFC);
+
+            Logger& logger = Logger::get(moduleName);
+            if (loglevel.compare("fatal") == 0) {
+                logger.setLevel(Poco::Message::PRIO_FATAL);
+            } else if (loglevel.compare("critical") == 0) {
+                logger.setLevel(Poco::Message::PRIO_CRITICAL);
+            } else if (loglevel.compare("error") == 0) {
+                logger.setLevel(Poco::Message::PRIO_ERROR);
+            } else if (loglevel.compare("warning") == 0) {
+                logger.setLevel(Poco::Message::PRIO_WARNING);
+            } else if (loglevel.compare("notice") == 0) {
+                logger.setLevel(Poco::Message::PRIO_NOTICE);
+            } else if (loglevel.compare("information") == 0) {
+                logger.setLevel(Poco::Message::PRIO_INFORMATION);
+            } else if (loglevel.compare("debug") == 0) {
+                logger.setLevel(Poco::Message::PRIO_DEBUG);
+            } else if (loglevel.compare("trace") == 0) {
+                logger.setLevel(Poco::Message::PRIO_TRACE);
+            }
+
+            logger.information("DemoSIM IP = " + ip + " / DemoSIM Port = "
+                    + NumberFormatter::format(port));
+
+            authid = 1;
+
+            DBUpdate dbupd(moduleName, logger);
+            Poco::Thread thread2;
+            thread2.start(dbupd);
+
+            HTTPServerParams* pParams = new HTTPServerParams;
+            pParams->setKeepAlive(false);
+            //pParams->setMaxThreads(maxThreads);
+            pParams->setMaxQueued(maxQueued);
+            //pParams->setThreadIdleTime(threadIdleTime);
+
+            ServerSocket svs(port, maxConns);
+            //svs.setReuseAddress(true);
+
+            Poco::ThreadPool th(2, maxThreads, 60, 0);
+
+            HTTPServer s(new RequestHandlerFactory(logger), th,
+                    svs, pParams);
+
+            s.start();
+
+            logger.information("Server Started");
+
+            waitForTerminationRequest(); // wait for CTRL-C or kill
+
+            logger.information("Shutting down");
+            s.stopAll(true); // Forcefully close all the connections
+        }
+
+        return Application::EXIT_OK;
+    }
+
+private:
+    bool _helpRequested;
+};
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char** argv) {
+    DemoServerApp app;
+
+    return app.run(argc, argv);
+}
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=/home/abhishek
+CND_ARTIFACT_NAME_Debug=DemoSIM
+CND_ARTIFACT_PATH_Debug=/home/abhishek/DemoSIM
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=demosim.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/demosim.tar
+# Release configuration
+CND_PLATFORM_Release=None-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/None-Linux
+CND_ARTIFACT_NAME_Release=demosim
+CND_ARTIFACT_PATH_Release=dist/Release/None-Linux/demosim
+CND_PACKAGE_DIR_Release=dist/Release/None-Linux/package
+CND_PACKAGE_NAME_Release=demosim.tar
+CND_PACKAGE_PATH_Release=dist/Release/None-Linux/package/demosim.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
